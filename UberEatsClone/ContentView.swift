01a15200d1dc8ecdf3d7bf91025649b4f38f87c8
Marina A <25265018+MarinaBSA@users.noreply.github.com>
2021-06-14 23:11:41 +0200

    Initial Commit

---
+++ UberEatsClone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UberEatsClone
+//
+//  Created by Marina Beatriz Santana de Aguiar on 14.06.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UberEatsClone/UberEatsCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberEatsCloneApp.swift
+//  UberEatsClone
+//
+//  Created by Marina Beatriz Santana de Aguiar on 14.06.21.
+//
+
+import SwiftUI
+
+@main
+struct UberEatsCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
